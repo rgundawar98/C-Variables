0da9c7193fde6c45a3f063f2f3ffac807c200346
rahulgundawar98 <rahulgundawar98@gmail.com>
2022-12-08 20:31:37 +0530

    [Rahul] Adding a Static variables into the Github

--- CSharpVariables/Program.cs
+++ CSharpVariables/Program.cs
@@ -7,19 +7,10 @@ namespace CSharpVariables
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            LocalVariable.StudentAge();
-            LocalVariables1.StudentAge();
-            InstanceVariables obj1= new InstanceVariables();
-            obj1.EngMarks = 75;
-            obj1.MarMarks = 90;
-            obj1.HindiMarks = 80;
-            obj1.StudentMarks();
+         
 
-            InstanceVariables obj2 = new InstanceVariables();
-            obj2.EngMarks = 80;
-            obj2.MarMarks = 94;
-            obj2.HindiMarks = 80;
-            obj2.StudentMarks();
+            StaticVariables.salary = 24500;
+            Console.WriteLine("Employee name is " +StaticVariables.name + " and his minimum salary is " + StaticVariables.salary);
             Console.ReadLine();
         }
     }
