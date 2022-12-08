5977b4f5b15d46f1999ca922b455cabe6c05e8b9
rahulgundawar98 <rahulgundawar98@gmail.com>
2022-12-08 19:39:16 +0530

    [Rahul]Adding instance of variables

--- CSharpVariables/Program.cs
+++ CSharpVariables/Program.cs
@@ -9,6 +9,17 @@ namespace CSharpVariables
             Console.WriteLine("Hello World!");
             LocalVariable.StudentAge();
             LocalVariables1.StudentAge();
+            InstanceVariables obj1= new InstanceVariables();
+            obj1.EngMarks = 75;
+            obj1.MarMarks = 90;
+            obj1.HindiMarks = 80;
+            obj1.StudentMarks();
+
+            InstanceVariables obj2 = new InstanceVariables();
+            obj2.EngMarks = 80;
+            obj2.MarMarks = 94;
+            obj2.HindiMarks = 80;
+            obj2.StudentMarks();
             Console.ReadLine();
         }
     }
